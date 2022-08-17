0bf5145d6a7d00ec1d936bda9a2cacd0664bd319
zxcvego <zxcvego@gmail.com>
2022-08-18 01:16:22 +0200

    Basic homepage

--- basic-react-apps/src/App.tsx
+++ basic-react-apps/src/App.tsx
@@ -1,7 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	return null;
+	return (
+		<div>
+			<img src={logo} alt="logo" className="app-logo" />
+			<h1>Basic React.js Applications</h1>
+			<div className="select-box">
+				<h2>ToDo List</h2>
+			</div>
+			<div className="select-box">
+				<h2>Habit Tracker</h2>
+			</div>
+			<div className="select-box">
+				<h2>Tic Tac Toe</h2>
+			</div>
+			<div className="select-box">
+				<h2>TBA</h2>
+			</div>
+		</div>
+	);
 }
 
 export default App;
