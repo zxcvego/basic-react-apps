b00cc27b7e5a71bdae613866b83d0d9f0a2958b4
zxcvego <zxcvego@gmail.com>
2022-08-19 18:49:31 +0200

    Add grid for buttons in HomeMenu

--- basic-react-apps/src/components/HomeMenu.jsx
+++ basic-react-apps/src/components/HomeMenu.jsx
@@ -3,18 +3,20 @@ import "./HomeMenu.css";
 export default function HomeMenu() {
 	return (
 		<>
-			<button className="select-box">
-				<h1>ToDo List</h1>
-			</button>
-			<button className="select-box">
-				<h1>Habit Tracker</h1>
-			</button>
-			<button className="select-box">
-				<h1>Tic Tac Toe</h1>
-			</button>
-			<button className="select-box">
-				<h1>Weather</h1>
-			</button>
+			<div className="grid-container">
+				<button className="select-box">
+					<h1>ToDo List</h1>
+				</button>
+				<button className="select-box">
+					<h1>Habit Tracker</h1>
+				</button>
+				<button className="select-box">
+					<h1>Tic Tac Toe</h1>
+				</button>
+				<button className="select-box">
+					<h1>Weather</h1>
+				</button>
+			</div>
 		</>
 	);
 }
