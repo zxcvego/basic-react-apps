a0d70b2178a66e52535a1999070b742d6bc7b81c
zxcvego <zxcvego@gmail.com>
2022-08-19 19:21:58 +0200

    useNavigate hook redirecting to each app

--- basic-react-apps/src/components/HomeMenu.jsx
+++ basic-react-apps/src/components/HomeMenu.jsx
@@ -1,19 +1,25 @@
 import "./HomeMenu.css";
+import { useNavigate } from "react-router-dom";
 
 export default function HomeMenu() {
+	const navigate = useNavigate();
+
 	return (
 		<>
 			<div className="grid-container">
-				<button className="select-box">
+				<button className="select-box" onClick={() => navigate("/todo-list")}>
 					<h1>ToDo List</h1>
 				</button>
-				<button className="select-box">
+				<button
+					className="select-box"
+					onClick={() => navigate("/habit-tracker")}
+				>
 					<h1>Habit Tracker</h1>
 				</button>
-				<button className="select-box">
+				<button className="select-box" onClick={() => navigate("/tic-tac-toe")}>
 					<h1>Tic Tac Toe</h1>
 				</button>
-				<button className="select-box">
+				<button className="select-box" onClick={() => navigate("/weather")}>
 					<h1>Weather</h1>
 				</button>
 			</div>
