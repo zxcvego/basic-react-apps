8d0843533ad2574f6d2f7ba19a30edfdf87b87eb
zxcvego <zxcvego@gmail.com>
2022-08-19 18:20:48 +0200

    HomeMenu divs to buttons

--- basic-react-apps/src/components/HomeMenu.jsx
+++ basic-react-apps/src/components/HomeMenu.jsx
@@ -3,18 +3,18 @@ import "./HomeMenu.css";
 export default function HomeMenu() {
 	return (
 		<>
-			<div className="select-box">
-				<h2>ToDo List</h2>
-			</div>
-			<div className="select-box">
-				<h2>Habit Tracker</h2>
-			</div>
-			<div className="select-box">
-				<h2>Tic Tac Toe</h2>
-			</div>
-			<div className="select-box">
-				<h2>Weather</h2>
-			</div>
+			<button className="select-box">
+				<h1>ToDo List</h1>
+			</button>
+			<button className="select-box">
+				<h1>Habit Tracker</h1>
+			</button>
+			<button className="select-box">
+				<h1>Tic Tac Toe</h1>
+			</button>
+			<button className="select-box">
+				<h1>Weather</h1>
+			</button>
 		</>
 	);
 }
